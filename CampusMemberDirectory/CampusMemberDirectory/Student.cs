46bb3a4ad8cc53df8164f6aff3f5c59a1171d549
madhavpnair <madhavpnair707@gmail.com>
2026-08-29 06:00:12 +0530

    implement Student class

---
+++ CampusMemberDirectory/CampusMemberDirectory/Student.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CampusMemberDirectory;
+
+public sealed class Student : CampusMember
+{
+    public Student(string name, string programme) : base(name)
+    {
+        Programme = programme;
+    }
+
+    public string Programme { get; }
+    public override string DescribeRole()
+    {
+        return $"Student in {Programme}";
+    }
+
+
+
+}
