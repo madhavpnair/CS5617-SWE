d2a006833a99e121ce36cd7487e6a77d2ea08871
madhavpnair <madhavpnair707@gmail.com>
2026-08-29 06:02:00 +0530

    implement Lecturer class

---
+++ CampusMemberDirectory/CampusMemberDirectory/Lecturer.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CampusMemberDirectory;
+
+public sealed class Lecturer : CampusMember
+{
+    public Lecturer(string name, string department) : base(name)
+    {
+        Department = department;
+    }
+
+    public string Department { get; }
+
+    public override string DescribeRole()
+    {
+        return $"Lecturer in {Department}";
+    }
+}
