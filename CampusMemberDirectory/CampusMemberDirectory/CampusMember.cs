5f19e07e76f42d7a8126b5f52787d63863c1d471
madhavpnair <madhavpnair707@gmail.com>
2026-08-29 06:49:19 +0530

    throw exception for empty name

--- CampusMemberDirectory/CampusMemberDirectory/CampusMember.cs
+++ CampusMemberDirectory/CampusMemberDirectory/CampusMember.cs
@@ -4,6 +4,11 @@ public abstract class CampusMember
 {
     protected CampusMember(string name)
     {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Name cannot be null or whitespace.", nameof(name));
+        }
+
         Name = name;
     }
 
